c6509c952fe3722e576af04a5a4d889ca4965e90
Abdelouahab Kemmoune <akemmoun@e2r6p9.1337.ma>
2025-04-12 15:33:15 +0100

    first commit

---
+++ minishell.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akemmoun <akemmoun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/11 19:32:00 by akemmoun          #+#    #+#             */
+/*   Updated: 2025/04/12 14:54:33 by akemmoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <readline/readline.h>
+// #include <readline/history.h>
+
+typedef enum e_token_type
+{
+	WORD,
+	PIPE,
+	REDIR_IN,
+	REDIR_OUT,
+	HEREDOC,
+	APPEND,
+}	t_token_type;
+
+typedef struct s_token
+{
+	char *value;
+	t_token_type type;
+	struct s_token *next;
+}	t_token;
+
+
+
+# endif
