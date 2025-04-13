57dadebdb7193323952dc80fe41ba52e150e44a0
Abdelouahab Kemmoune <akemmoun@e2r6p4.1337.ma>
2025-04-13 18:03:10 +0100

    commit n3

--- lexer_errors.c
+++ lexer_errors.c
@@ -1,46 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   lexer_errors.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akemmoun <akemmoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 19:32:00 by akemmoun          #+#    #+#             */
-/*   Updated: 2025/04/13 17:17:43 by akemmoun         ###   ########.fr       */
+/*   Updated: 2025/04/13 17:14:19 by akemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef MINISHELL_H
-# define MINISHELL_H
+#include "minishell.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <readline/readline.h>
-#include "libft.h"
-// #include <readline/history.h>
-
-typedef enum e_token_type
+int is_quote_closed(char *input, char quote, int start)
 {
-	WORD,
-	PIPE,
-	REDIR_IN,
-	REDIR_OUT,
-	HEREDOC,
-	APPEND,
-}	t_token_type;
+	while (input[start])
+	{
+		if (input[start] == quote)
+			return (1);
+		start++;
+	}
+	return (0);
+}
 
-typedef struct s_token
+void print_error(char *msg)
 {
-	char *value;
-	t_token_type type;
-	struct s_token *next;
-}	t_token;
-
-
-/*-------------errors-------------*/
-
-int is_quote_closed(char *input, char quote, int start);
-void print_error(char *msg);
-
-# endif
+	ft_putstr_fd("Lexer error: ", 2);
+	ft_putstr_fd(msg, 2);
+	ft_putchar_fd('\n', 2);
+}
